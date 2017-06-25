af0a6e72e5e59b7f4e71dce5e9bf32afb81875a0
catkins <catkinsjr28@gmail.com>
2017-06-25 09:59:40 -0400

    add animation

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-    //array of sports
+//array of sports
     topics = ["Basketball", "Baseball", "Football", "NASCAR", "Hockey", "Soccer", "MMA", "Boxing", "Lacrosse", "Golf", "Tennis", "Swimming", "Track & Field"];
-    //creates buttons by looping throughusing the strings from the topics array
+
+//creates buttons by looping throughusing the strings from the topics array
     function sports() {
-        // clears the array before adding new strings to it
+
+// clears the array before adding new strings to it
         $("#buttons").empty();
-        // loops through topics array
+
+// loops through topics array
         for (var i = 0; i < topics.length; i++) {
             var buttons = $("<button>");
             buttons.addClass("sports");
@@ -15,8 +18,10 @@ $(document).ready(function() {
         }
     };
     sports();
-    //when button is clicked return 10 hits from giphy API search including still gif and rating
-    $(".sports").click(function() {
+
+//when button is clicked return 10 hits from giphy search search including still gif and rating
+    function sportButtonClick(){
+        $(".sports").click(function() {
         var sport = $(this).attr("data-name");
         var apiKey = "45657ac5520149fb807bcf8f97c84b60";
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=" + apiKey + "&limit=10";
@@ -29,8 +34,11 @@ $(document).ready(function() {
                 var giphyDiv = $("<div class='item'>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var sportImage = $("<img>");
+                var sportImage = $("<img>").addClass("soloGif");
                 sportImage.attr("src", results[i].images.fixed_height_still.url);
+                sportImage.attr({
+                    "data-state": "animate"
+                });
                 sportImage.attr({
                     "data-animate": results[i].images.fixed_height.url
                 });
@@ -46,14 +54,38 @@ $(document).ready(function() {
             };
         });
     });
-    $(".item").click(function() {
+    };
+    sportButtonClick();
+
+//click any still gif and it will animate. else if it is animating it will become still
+    $(".soloGif").click(function() {
         var state = $(this).attr('data-state');
         if (state == "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
-        } else {
+        } 
+        else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
     });
+
+//when there is a value in the input field, once you click submit, the value will be placed at the  
+//top in a new button which can be clicked and will return 10 results with a still gif and rating
+    $("#searchButton").click(function(){
+       var addTopic =  $("#search").val().trim();
+       topics.push(addTopic);
+       sports();
+       sportButtonClick();
+//each time the submit button is clicked, clear the text input field
+       $("#form").each(function(){
+    this.reset();
+});
+    });
+
+
+
+
+
+
 });
